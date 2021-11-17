d21313ab7bb9d80ec3458c10568d3d523120947d
Emad Alghamdi <37039039+Eaalghamdi@users.noreply.github.com>
2021-11-17 23:16:14 +0300

    created env

--- backend/api_server.py
+++ backend/api_server.py
@@ -1,20 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
 
-# from FeatureExtraction.Utils import Processing
-# import FeatureExtraction
-
-# from fastapi import FastAPI, Request, Depends, BackgroundTasks, File, UploadFile
-# from pydantic import BaseModel
-# from schemas import ProjectRequest
-# import sqlalchemy
-# import models
-# from db import SessionLocal, engine
-# from sqlalchemy.orm import Session
-# from models import Project
-# import os
-# import sys
-# from glob import glob
 
 
 if __name__ == '__main__':
