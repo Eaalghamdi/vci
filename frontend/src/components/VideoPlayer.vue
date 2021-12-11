eaac0df18ee105c51e3a77fd119f8626a2ac1145
Emad Alghamdi <37039039+Eaalghamdi@users.noreply.github.com>
2021-12-11 12:56:25 +0300

    update npm lib

--- frontend/src/components/VideoPlayer.vue
+++ frontend/src/components/VideoPlayer.vue
@@ -6,7 +6,7 @@
     controls
     data-poster="/path/to/poster.jpg"
   >
-    <source src="" type="video/mp4" />
+    <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4" />
   </video>
 </template>
 <script>
