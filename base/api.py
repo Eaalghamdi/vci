b2466f70e7e85eff4611c981ebd8cbd189cd1166
NickyNm <81171050+Nickynm@users.noreply.github.com>
2022-02-08 06:50:06 +0530

    updated

--- base/api.py
+++ base/api.py
@@ -1,12 +1,10 @@
 import provider as Provider
 import sys
 import database.db as DB
-import cv2
 
 
 if __name__ == '__main__':
     if sys.argv[1] == 'init':
-        print(cv2.__file__)
         print(DB.init())
         sys.stdout.flush()
     elif sys.argv[1] == 'get_projects':
@@ -24,14 +22,14 @@ if __name__ == '__main__':
         id = [sys.argv[2]]
         print(Provider.delete_project(id))
         sys.stdout.flush()
-    # elif sys.argv[1] == 'shot_bundry_detection':
-    #     videoPath = sys.argv[2]
-    #     videoFileName = sys.argv[3]
-    #     method = sys.argv[4]
-    #     treshold = sys.argv[5]
-    #     print(Provider.fe_shot_boundry_det(
-    #         videoPath, videoFileName, method, treshold))
-    #     sys.stdout.flush()
+    elif sys.argv[1] == 'shot_bundry_detection':
+        videoPath = sys.argv[2]
+        videoFileName = sys.argv[3]
+        method = sys.argv[4]
+        treshold = sys.argv[5]
+        print(Provider.fe_shot_boundry_det(
+            videoPath, videoFileName, method, treshold))
+        sys.stdout.flush()
     elif sys.argv[1] == 'manual_frame':
         videoPath = sys.argv[2]
         videoFileName = sys.argv[3]
